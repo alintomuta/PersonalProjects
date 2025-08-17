90e3cab82e2fb477d59667fa7197742282ef96a9
Alin Tomuta <73099266+alintomuta@users.noreply.github.com>
2025-08-17 09:36:28 +0300

    Add files via upload
    geometric shape using turtle

---
+++ spirograph.py
@@ -0,0 +1,14 @@
+import random
+import turtle as t
+
+
+t.shape('arrow')
+t.color('red','violet')
+t.speed('fastest')
+for _ in range(99):
+     for _ in range(18):
+        t.right(20)
+        t.forward(35)
+     t.right(5)
+
+
